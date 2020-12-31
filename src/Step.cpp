7912cdc13e6c1ec54c1f26b0f9c3b34d38111502
2000DED <44520488+2000DED@users.noreply.github.com>
2020-12-31 04:34:44 +0300

    Add files via upload

--- src/Step.cpp
+++ src/Step.cpp
@@ -1,14 +1,154 @@
-#include "Step.h"  // подключаем заголовок обязательно
-// реализация методов
-void Motor::setMotor(int motor_pin_Step, int motor_pin_Dir)
+#include "Step.h"
+
+void Motor::setMotor(float maxium, int stp, float coefficient, int StepD = 800, bool TurnDirection = 1, float S = 0)
+{
+  stepsPerTurn = stp;
+  k = coefficient;
+  Dir = TurnDirection;
+  min_step_delay = StepD;
+  lenA = (maxium / k) * stepsPerTurn;
+  posA = (S / k) * stepsPerTurn;
+  pos = posA;
+
+  pinMode(_pin_E, OUTPUT);
+  pinMode(_pin_Step, OUTPUT);
+  pinMode(_pin_Dir, OUTPUT);
+  digitalWrite(_pin_E, 0);
+  digitalWrite(_pin_Dir, Dir);
+}
+
+void Motor::setPins(int motor_pin_Step, int motor_pin_Dir, int motor_pin_E)
 {
   _pin_Step = motor_pin_Step;
   _pin_Dir = motor_pin_Dir;
+  _pin_E = motor_pin_E;
 
-  pinMode(8, OUTPUT);
+  pinMode(_pin_E, OUTPUT);
   pinMode(_pin_Step, OUTPUT);
   pinMode(_pin_Dir, OUTPUT);
-  digitalWrite(8, 0);
+  digitalWrite(_pin_E, 0);
+  digitalWrite(_pin_Dir, Dir);
+}
+
+float Motor::getPositionA()
+{
+  return ((float)posA / stepsPerTurn) * k;
+}
+
+float Motor::getPositionR()
+{
+  return ((float)pos / stepsPerTurn) * k;
+}
+
+void Motor::goHome()
+{
+  if (!coordinate_system)
+  {
+    setDir(!Dir);
+
+    steps(posA);
+    posA = 0;
+    pos = -start;
+
+    setDir(Dir);
+  }
+  else
+  {
+    if (pos > 0)
+      setDir(!Dir);
+
+    steps(pos);
+    pos = 0;
+    posA = start;
+
+    setDir(Dir);
+  }
+}
+
+void Motor::absolute()
+{
+  coordinate_system = 0;
+}
+
+void Motor::relative()
+{
+  coordinate_system = 1;
+}
+
+void Motor::goLength(int S)
+{
+  int number_of_steps = (S / k) * stepsPerTurn;
+
+  if (!coordinate_system)
+  {
+    if (number_of_steps < posA)
+      setDir(!Dir);
+
+    if (number_of_steps <= lenA && number_of_steps >= 0)
+    {
+      steps(posA - number_of_steps);
+      posA = number_of_steps;
+      pos = posA - start;
+    }
+
+    setDir(Dir);
+  }
+  else
+  {
+    if (number_of_steps < pos)
+      setDir(!Dir);
+
+    if (start + number_of_steps <= lenA && start + number_of_steps >= 0)
+    {
+      steps(pos - number_of_steps);
+      posA = start + number_of_steps;
+      pos = number_of_steps;
+    }
+
+    setDir(Dir);
+  }
+}
+
+void Motor::plusLength(int S)
+{
+  int number_of_steps = (S / k) * stepsPerTurn;
+
+  check(number_of_steps);
+}
+
+void Motor::plusAngle(float a)
+{
+  int number_of_steps = round((a / 360.0) * stepsPerTurn);
+
+  check(number_of_steps);
+}
+
+void Motor::plusTurn(float a)
+{
+  int number_of_steps = round(a * stepsPerTurn);
+
+  check(number_of_steps);
+}
+
+void Motor::check(int k)
+{
+  if (k < 0)
+    setDir(!Dir);
+
+  if (posA + k <= lenA && posA + k >= 0)
+  {
+    steps(k);
+    posA += k;
+    pos += k;
+  }
+
+  setDir(Dir);
+}
+
+void Motor::zero()
+{
+  start = posA;
+  pos = 0;
 }
 
 void Motor::setDir(bool whatDir)
@@ -16,14 +156,15 @@ void Motor::setDir(bool whatDir)
   digitalWrite(_pin_Dir, whatDir);
 }
 
-void Motor::setSpd(long whatSpeed)
+void Motor::setSpeedPercent(float whatSpeed)
 {
-  step_delay = whatSpeed;
+  whatSpeed = constrain(whatSpeed, 0, 100);
+  step_delay = round(map(whatSpeed, 0, 100, MAX_STEP_DELAY, min_step_delay));
 }
 
-void Motor::go(long kolvo_steps)
+void Motor::steps(long kolvo_steps)
 {
-  for (long i = 0; i < kolvo_steps; i++)
+  for (long i = 0; i < abs(kolvo_steps); i++)
   {
     digitalWrite(_pin_Step, 1);
     digitalWrite(_pin_Step, 0);
--- src/Step.h
+++ src/Step.h
@@ -1,25 +1,56 @@
 #pragma once
 #include <Arduino.h>
-// описание класса
+#define PIN_S 2
+#define PIN_D 5
+#define PIN_E 8
+#define STEPS_PER_TURN 200
+#define MAX_STEP_DELAY 5000
+#define STEP_DELAY 800
+#define COF 1.245
+
 class Motor {
   public:
-    // constructors:
-    void setMotor(int motor_pin_Step, int motor_pin_Dir);
 
-    void setDir(bool whatDir);
+    // Обявление шагового мотора(максимальная длина хода в mm, шагов в обороте, отношение mm к обороту, минимальная задержка в mcs между шагами, направление вращения, изначальная позиция)
+    void setMotor(float maxium, int stp, float coefficient, int StepD = 800, bool TurnDirection = 1, float S = 0);
 
-    // speed setter method:
-    void setSpd(long whatSpd);
+    // Обявление пинов шагового мотора(пин импульса, пин направления, пин включения драйвера)
+    void setPins(int motor_pin_Step, int motor_pin_Dir, int motor_pin_E);
 
-    // mover method:
-    void go(long kolvo_steps);
+    void setSpeedPercent(float whatSpd);  // Выбор скорости в процентах
+
+    float getPositionA(); // Возвращает позицию в абсолютной системе координат
+    float getPositionR(); // Возвращает позицию в относительной системе координат
+
+    void absolute();  // Переключение на абсолютную систему координат
+    void relative();  // Переключение на относительную систему координат
+
+    void goHome();          // Перемещение в начальную позицию
+    void goLength(int S);   // Перемещение в заданную позицию
+
+    void plusLength(int S);   // Перемещение на длину
+    void plusAngle(float a);  // Перемещение на угол
+    void plusTurn(float a);   // Перемещение на оборот
+
+    void zero();  // Обнуление относительной системы координат
 
   private:
+    void check(int k);  // Проверка на невыход за пределы абсолютной системе координат
 
-    unsigned long step_delay = 800; // delay between steps, in ms, based on speed
-    long _steps = 100;
+    void setDir(bool whatDir);    // Задает направление движения мотора
+    void steps(long kolvo_steps); // Шагает мотором
 
-    int _pin_Step = 2;
-    int _pin_Dir = 5;
+    bool Dir = 1;                       // Направление движения
+    bool coordinate_system = 0;         // Абсолютная или относительная системе координат
+    long min_step_delay = STEP_DELAY;   // Минимальная задержка в mcs между шагами
+    long step_delay = STEP_DELAY;       // Задержка в mcs между шагами
+    int stepsPerTurn = STEPS_PER_TURN;  // Шагов в обороте
+    float k = COF;                      // Отношение mm к обороту
 
+    long lenA = 10, posA = 0; // Максимальная длина хода в mm, Абсолютная позиция
+    long start = 0, pos = 0;  // Растояние от начала абсолютной до начала относительной системы координат
+
+    int _pin_Step = PIN_S;    // Пин импульса
+    int _pin_Dir = PIN_D;     // Пин направления
+    int _pin_E = PIN_E;       // Пин включения драйвера
 };
